a4e43c4bab2b99bfd9975f14b2788f897c4504de
JethroSama <kel73896@gmail.com>
2021-12-26 00:08:21 +0800

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
