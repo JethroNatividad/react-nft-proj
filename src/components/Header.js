35fac5fd4ebd057805e86350ae3c97719dac09cf
JethroSama <kel73896@gmail.com>
2021-12-26 01:14:54 +0800

    cleanup css classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,38 +4,38 @@ import searchIcon from '../assets/header/search.png'
 import themeSwitchIcon from '../assets/header/theme-switch.png'
 const Header = () => {
     return (
-        <div className="text-offWhite p-5 flex justify-between items-center" >
+        <div className="header" >
             {/* Logo */}
-            <div className="w-44 mr-4">
+            <div className="w-44">
                 <img src={logo} alt="logo" className="w-full" />
             </div>
 
             {/* Search */}
-            <div className="w-64 h-12 flex items-center bg-gray-600 rounded-3xl flex-1 p-2">
+            <div className="search">
                 <div className="w-6 object-cover">
                     <img src={searchIcon} alt="search" className="w-full h-full" />
                 </div>
-                <input type="text" className="border-none p-2 outline-none bg-transparent" placeholder="Search" />
+                <input type="text" className="search-input" placeholder="Search" />
             </div>
 
             {/* Misc */}
 
-            <div className="flex text-gray-400 justify-evenly">
-                <p className="mx-3 cursor-pointer hover:text-offWhite">Drops</p>
-                <p className="mx-3 cursor-pointer hover:text-offWhite">Marketplace</p>
-                <p className="mx-3 cursor-pointer hover:text-offWhite">Create</p>
+            <div className="misc">
+                <p className="misc-item">Drops</p>
+                <p className="misc-item">Marketplace</p>
+                <p className="misc-item">Create</p>
             </div>
 
             {/* Actions */}
 
             <div className="flex items-center">
-                <div className="bg-gray-600 rounded-3xl h-12 w-12 flex items-center justify-center mx-6">
+                <div className="theme-switch">
                     <div className="w-8 object-cover">
                         <img src={themeSwitchIcon} alt="toggle" className="w-full h-full" />
                     </div>
                 </div>
 
-                <div className="bg-gradient-to-r from-green-300 to-cyan-400 h-12 w-28 rounded-3xl flex justify-center items-center mx-3">
+                <div className="login-button">
                     <p>LOGIN</p>
                 </div>
 
