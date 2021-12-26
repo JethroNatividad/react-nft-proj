b465a7c43cce1a14feb764d59583627664b9e72e
JethroSama <kel73896@gmail.com>
2021-12-26 17:32:17 +0800

    Add axios

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,14 @@ import React from 'react'
 import ethIcon from '../assets/eth.png'
 const Card = ({ img, title, price, id }) => {
     return (
-        <div className="bg-gray-600 text-offWhite w-60 rounded-xl shadow-md shadow-gray-600">
-            <img src={img} alt={title} className="w-full object-cover rounded-tr-xl rounded-tl-xl" />
+        <div className="bg-gray-600 text-offWhite w-60 rounded-xl shadow-md shadow-gray-600 overflow-hidden">
+            <img src={img} alt={title} className="w-full object-cover" />
             {/* details */}
             <div className="p-5">
                 <p className="text-xl font-bold">{title}</p>
                 <p className="text-gray-300">{id}</p>
                 {/* price */}
-                <div className="flex mb-12">
+                <div className="flex">
                     <img className="w-6 object-contain ml-[-8px]" src={ethIcon} alt="eth" />
                     <p>{price}</p>
                 </div>
