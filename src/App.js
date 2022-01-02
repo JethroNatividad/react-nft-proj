ec7a312ccd9770150bb59bcd1de5adeb90007496
JethroSama <kel73896@gmail.com>
2022-01-02 18:48:47 +0800

    Add loading

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Header from './components/Header';
 import axios from 'axios'
 import CardList from './components/CardList';
-
+import spinner from './assets/loading.svg'
 function App() {
   const [nfts, setNfts] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     async function main() {
@@ -13,12 +14,15 @@ function App() {
       const collectionAddress = '0x7DC2bce2eE2d4BC1a65c00C56516691B913D0d1D'
       const res = await axios.get(`https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=${collectionAddress}`)
       setNfts(res.data.assets)
+      setLoading(false)
     }
     return main()
   }, [])
 
   return (
     <div className="h-screen w-screen bg-black font-sans">
+
+      {loading && <div className="absolute w-full h-full flex justify-center items-center"><img src={spinner} alt="loading" /></div>}
       <Header />
       <CardList nfts={nfts} />
     </div>
