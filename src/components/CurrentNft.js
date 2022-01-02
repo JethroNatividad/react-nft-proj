7f5db18fc7e852bc2002c23cdd68b37be23aefac
JethroSama <kel73896@gmail.com>
2022-01-02 19:23:28 +0800

    change dot and color gray

--- src/components/CurrentNft.js
+++ src/components/CurrentNft.js
@@ -7,7 +7,7 @@ const CurrentNft = () => {
                 <img className="h-60 object-contain rounded-xl" src="https://ipfs.io/ipfs/bafkreigehmswocff4fwv4ktbvs7bfj2eliledu2ofegi664fxraaowzjwq" alt="current" />
             </div>
             <div>
-                <h1 className="text-white font-bold text-6xl">BEST GODDESS<span>·#1</span></h1>
+                <h1 className="text-white font-bold text-6xl">BEST GODDESS<span className="text-gray-300">·#1</span></h1>
             </div>
 
         </div>
