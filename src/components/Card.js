429542181d05d9bf66cb6ee892bfd8776f537e39
JethroSama <kel73896@gmail.com>
2022-01-02 23:52:14 +0800

    add all dark/white modes + transition

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,11 @@ import React from 'react'
 import ethIcon from '../assets/eth.png'
 const Card = ({ img, title, traits, id, setCurrentNft, nft }) => {
     return (
-        <div onClick={() => setCurrentNft(nft)} className="bg-gray-600 text-offWhite w-60 h-[400px] rounded-xl shadow-md shadow-gray-600 overflow-hidden mx-3 shrink-0 cursor-pointer">
+        <div onClick={() => setCurrentNft(nft)} className="bg-offWhite-100 dark:bg-gray-600 text-offWhite w-60 h-[400px] rounded-xl shadow-md shadow-gray-600 overflow-hidden mx-3 shrink-0 cursor-pointer my-5">
             <img src={img} alt={title} className="w-full object-cover" />
             {/* details */}
-            <div className="p-5">
-                <p className="text-xl font-bold">{title}</p>
+            <div className="p-5 dark:text-white">
+                <p className=" text-xl font-bold">{title}</p>
                 <p className="text-gray-300">·#{id}</p>
                 {/* price */}
                 <div className="flex">
